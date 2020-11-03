b027ad37d0a150e81081bfa33b5f70be7247bc09
Vladzensan <vladzensan@gmail.com>
2020-11-03 22:11:44 +0300

    Add test for extension methods

---
+++ ExtensionMethodTest/NewClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OtherNamespaceTest
+{
+    public class NewClass
+    {
+        public int num;
+    }
+}
---
+++ ExtensionMethodTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ExtensionMethodTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Useful useful = new Useful();
+            Console.WriteLine(useful.GetExtension(10));
+            
+        }
+    }
+
+    public class Useful
+    {
+        public void SomeUsefulMethod()
+        {
+
+        }
+    }
+
+    public static class Extension
+    {
+        public static int GetExtension(this Useful useful, int num)
+        {
+            return num;
+        }
+    }
+}
